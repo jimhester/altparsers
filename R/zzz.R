90a5c333825b3595c8513a0049879c7fd35efe74
Jim Hester <james.f.hester@gmail.com>
2018-06-18 16:07:15 -0400

    Remove unused code
    I only need the parser code for altparse

--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-#' @importFrom utils data
-#' @import Rcpp 
-#' @useDynLib parser, .registration = TRUE
-.onLoad <- function( libname, pkgname ){
-    # loadRcppModules()
-	#loadModule("parser_module", T)
-}
